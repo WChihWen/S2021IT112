959a4963d54f6a3490b7ad704b4004f39574e869
WChihWen <yaman0923@gmail.com>
2021-05-12 10:36:27 -0700

    resource view commit

--- Club/views.py
+++ Club/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Resource, User
 
 # Create your views here.
 def index(request):
     return render(request, 'Club/index.html')
+
+def resources(request):
+     return render(request, 'Club/resources.html', {'resource_list': Resource.objects.all()})
