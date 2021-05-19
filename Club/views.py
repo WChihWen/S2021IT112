a4b3423b93d6e35f430ba925b70a995eade9293f
WChihWen <yaman0923@gmail.com>
2021-05-19 15:06:19 -0700

    commit_meeting_detail

--- Club/views.py
+++ Club/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
-from .models import Resource, User
+from django.shortcuts import render, get_object_or_404
+from .models import Resource, User, Meeting
+from django.urls import reverse_lazy
 
 # Create your views here.
 def index(request):
     return render(request, 'Club/index.html')
 
 def resources(request):
-     return render(request, 'Club/resources.html', {'resource_list': Resource.objects.all()})
+    return render(request, 'Club/resources.html', {'resource_list': Resource.objects.all()})
+ 
+def meetings(request):
+    return render(request, 'Club/meetings.html', {'meeting_list': Meeting.objects.all()})
+ 
+def meetingdetails(request, id):
+    return render(request, 'Club/meetingdetails.html', {'meeting_list': get_object_or_404(Meeting, pk=id)})
+     
+     
