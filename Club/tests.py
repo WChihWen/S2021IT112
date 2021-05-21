f05805484ff7a3dcc0b657a897e6e5f8d1fdf595
WChihWen <yaman0923@gmail.com>
2021-05-20 17:29:17 -0700

    commit_test_models

--- Club/tests.py
+++ Club/tests.py
@@ -1,3 +1,63 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Meeting, Minutes, Resource, Event
+import datetime
 
-# Create your tests here.
+
+class MeetingTest(TestCase):
+    def setUp(self):
+        self.mtdate = datetime.date(2021,6,2)
+        self.mttime = datetime.time(12, 30, 00, 240000)
+        self.myMeeting = Meeting(meetingtitle = 'Optional IT112 week 10', meetingdate  = self.mtdate, meetingtime = self.mttime, location ='Zoon', Agenda = 'Q&A')
+        
+    def test_string(self):
+        self.assertEqual(str(self.myMeeting), 'Optional IT112 week 10')
+        
+    def test_table(self):
+       self.assertEqual(str(Meeting._meta.db_table), 'meeting')
+       
+
+class MinuteTest(TestCase):
+    def setUp(self):
+        self.mt = Meeting(meetingtitle = 'Optional IT112 week 10')
+        self.user = User(username = 'user01')
+        self.myMinutes = Minutes(meeting = self.mt, attendance = self.user, minutes = "Spending 25 minutes.")
+        
+    def test_string(self):
+        self.assertEqual(str(self.myMinutes), 'Spending 25 minutes.')
+        
+    def test_table(self):
+       self.assertEqual(str(Minutes._meta.db_table), 'minutes')
+    
+class ResourceTest(TestCase):
+    def setUp(self):
+        self.user = User(username = 'user01')
+        self.endate = datetime.date(2021,6,2)
+        self.url = 'https://www.w3schools.com/python/'
+        self.dcript = 'Python can be used on a server to create web applications.'
+        self.myresource = Resource(resourcename = 'Python Tutorial', resourcetype = 'Tutorial', resourceurl = self.url, 
+                                   entereddate = self.endate, user = self.user, description = self.dcript)
+                
+    def test_string(self):
+        self.assertEqual(str(self.myresource), 'Python Tutorial')
+        
+    def test_table(self):
+       self.assertEqual(str(Resource._meta.db_table), 'resource')
+
+
+class EventTest(TestCase):
+    def setUp(self):
+        self.ettile = 'Taking COVID Vaccine (2nd)'
+        self.etdate = datetime.date(2021,6,4)
+        self.ettime = datetime.time(10, 00, 00, 240000)
+        self.user = User(username = 'user01')
+        self.location = 'Ash Way Park & Ride DRIVE THROUGH - COVID Vaccine Site'
+        self.dcript ='This is second appointment for vaccination.'
+        self.myEvent = Event(eventtitle = self.ettile, eventdate = self.etdate, eventtime= self.ettime, 
+                             location = self.location, description = self.dcript, user = self.user)
+        
+    def test_string(self):
+        self.assertEqual(str(self.myEvent), 'Taking COVID Vaccine (2nd)')
+        
+    def test_table(self):
+       self.assertEqual(str(Event._meta.db_table), 'event')
